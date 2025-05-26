023fb1401f9c9e6d3ebf1775c8a8c73fafc8ce9b
arifrusmandani <arif.rusmandani@aqi.co.id>
2025-05-26 10:46:13 +0700

    Refactor member model to introduce Family

--- app/src/database/models/member.py
+++ app/src/database/models/member.py
@@ -1,17 +1,20 @@
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import Column, Integer, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 
 from app.src.database import BaseModel
-from app.src.database.models.user import User
 
 
-class Member(BaseModel):
-    __tablename__ = 'members'
+class EnumRelationship(enum.Enum):
+    spouse = 'spouse'
+
+class Family(BaseModel):
+    __tablename__ = 'Familys'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    member_user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    family_user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    relationship = Column(Enum(EnumRelationship), nullable=False)
 
     # Relationships
     user = relationship('User', foreign_keys=[user_id])
-    member_user = relationship('User', foreign_keys=[member_user_id])
+    family_user = relationship('User', foreign_keys=[family_user_id])
